bed40193ae3f3b2f49a09e9db696870915919f09
acacopardo <pookiie57@gmail.com>
2017-03-11 16:56:56 +0100

    Start add routing

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,11 @@
-<?php
+`<?php
 
 use FlashEvents\Controllers\Messages;
+use FlashEvents\Entities\Address;
+use FlashEvents\Entities\Event;
+use FlashEvents\Entities\Provider;
+use FlashEvents\Entities\Token;
+use FlashEvents\Entities\User;
 use Slim\Http\Request;
 use Slim\Http\Response;
 //use \Psr\Http\Message\ServerRequestInterface as Request;
@@ -15,12 +20,57 @@ $app->get('/', function (Request $request, Response $response) {
 });
 
 $app->group('/api', function () {
+    $this->group('/users', function () {
+        $this->get('[/]', User::class . ':getAll');
+        $this->get('/{id}', User::class . ':get');
+        $this->put('/{id}', User::class . ':update');
+        $this->post('[/]', User::class . ':create');
+        $this->delete('/{id}', User::class . ':delete');
+    });
 
-    $this->group('/messages', function () {
+    $this->group('/users/{id}/friends', function (){
+        $this->get('[/]', User::class . ':getAllFriends');
+        $this->get('/{id}', User::class . ':getFriend');
+        $this->post('[/]', User::class . ':addFriend');
+        $this->delete('[/]', User::class . ':deleteFriend');
+    });
 
-        $this->get('/{id}', Messages::class . ':get');
-        $this->get('[/]', Messages::class . ':getAll');
+    $this->group('/users/{id}/addresses', function (){
+        $this->get('[/]', User::class . ':getAllAddresses');
+        $this->get('/{id}', User::class . ':getAddress');
+        $this->post('[/]', User::class . ':addAddress');
+        $this->delete('[/]', User::class . ':deleteAddress');
+    });
 
-        $this->post('[/]', Messages::class . 'create');
+    $this->group('/addresses', function () {
+        $this->get('[/]', Address::class . ':getAll');
+        $this->get('/{id}', Address::class . ':get');
+        $this->put('/{id}', Address::class . ':update');
+        $this->post('[/]', Address::class . ':create');
+        $this->delete('/{id}', Address::class . ':delete');
+    });
+
+    $this->group('/events', function () {
+        $this->get('[/]', Event::class . ':getAll');
+        $this->get('/{id}', Event::class . ':get');
+        $this->put('/{id}', Event::class . ':update');
+        $this->post('[/]', Event::class . ':create');
+        $this->delete('/{id}', Event::class . ':delete');
+    });
+
+    $this->group('/providers', function () {
+        $this->get('[/]', Provider::class . ':getAll');
+        $this->get('/{id}', Provider::class . ':get');
+        $this->put('/{id}', Provider::class . ':update');
+        $this->post('[/]', Provider::class . ':create');
+        $this->delete('/{id}', Provider::class . ':delete');
+    });
+
+    $this->group('/tokens', function () {
+        $this->get('[/]', Token::class . ':getAll');
+        $this->get('/{id}', Token::class . ':get');
+        $this->put('/{id}', Token::class . ':update');
+        $this->post('[/]', Token::class . ':create');
+        $this->delete('/{id}', Token::class . ':delete');
     });
 });
--- src/Controllers/Users.php
+++ src/Controllers/Users.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jerome
- * Date: 10/03/2017
- * Time: 22:50
- */
 
 namespace FlashEvents\Controllers;
 
@@ -13,32 +7,39 @@ use Psr\Container\ContainerInterface;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-class Messages
+class Users
 {
     protected $container;
 
-    // constructor receives container instance
     public function __construct(ContainerInterface $container) {
         $this->container = $container;
     }
-
+    
     public function getAll(Request $request, Response $response) {
-        return $response->withJson(['messages' => [['id' => 1, 'text' => 'hello'], ['id' => 2, 'text' => 'all']]]);
+
     }
 
     public function get(Request $request, Response $response) {
 
-        $id = $request->getAttribute('id');
-
-        return $response->withJson(['id' => $id, 'text' => 'hello']);
-        //$res = $response->withHeader('Content-Type', 'application/json');
-        //return $res->getBody()->write(json_encode([]));
-
     }
 
     public function settings(Request $request, Response $response) {
-        $settings = $this->container->get('settings')['test'];
 
-        return $response->withJson([$settings, $this->container->get('service.test')->test()]);
+    }
+
+    public function getAllFriends(Request $request, Response $response) {
+
+    }
+
+    public function getFriend(Request $request, Response $response) {
+
+    }
+
+    public function addFriend(Request $request, Response $response) {
+
+    }
+
+    public function deleteFriend(Request $request, Response $response) {
+
     }
 }
