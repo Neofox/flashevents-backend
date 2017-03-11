708cef17060a801b12aaea5a34086ea5840eb4d8
acacopardo <pookiie57@gmail.com>
2017-03-11 15:36:08 +0100

    I

--- src/Entities/Address.php
+++ src/Entities/Address.php
@@ -40,4 +40,129 @@ class Address
      */
     protected $latitude;
 
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     * @return Address
+     */
+    public function setId(int $id): Address
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     * @return Address
+     */
+    public function setCity(string $city): Address
+    {
+        $this->city = $city;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getZipCode(): string
+    {
+        return $this->zipCode;
+    }
+
+    /**
+     * @param string $zipCode
+     * @return Address
+     */
+    public function setZipCode(string $zipCode): Address
+    {
+        $this->zipCode = $zipCode;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreetName(): string
+    {
+        return $this->streetName;
+    }
+
+    /**
+     * @param string $streetName
+     * @return Address
+     */
+    public function setStreetName(string $streetName): Address
+    {
+        $this->streetName = $streetName;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreetNumber(): string
+    {
+        return $this->streetNumber;
+    }
+
+    /**
+     * @param string $streetNumber
+     * @return Address
+     */
+    public function setStreetNumber(string $streetNumber): Address
+    {
+        $this->streetNumber = $streetNumber;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLongitude(): float
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param float $longitude
+     * @return Address
+     */
+    public function setLongitude(float $longitude): Address
+    {
+        $this->longitude = $longitude;
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLatitude(): float
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param float $latitude
+     * @return Address
+     */
+    public function setLatitude(float $latitude): Address
+    {
+        $this->latitude = $latitude;
+        return $this;
+    }
 }
--- src/Entities/Provider.php
+++ src/Entities/Provider.php
@@ -56,6 +56,4 @@ class Provider
         $this->name = $name;
         return $this;
     }
-
-
 }
